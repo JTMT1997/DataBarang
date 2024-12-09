e4b52ef2835c5d524585a512dbaca8c61dd03dcb
Jeremy Tambunan <Jhie.jhie@live.com>
2024-12-09 07:29:19 +0700

    tugasWebPBW

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+  $host = "localhost"; 
+  $user = "id20235035_root";
+  $pass = "6K^p3>Z+bQDA[p3c";
+  $nama_db = "id20235035_kantin_unsia"; //nama database
+  $koneksi = mysqli_connect($host,$user,$pass,$nama_db); 
+
+
+?>
